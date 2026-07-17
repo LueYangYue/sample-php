e869a541e9dddbf3c88af2593039b64dcee5b9fa
LueYangYue <a202211@siswa.ukm.edu.my>
2026-07-18 01:38:36 +0800

    add message

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use Cowsayphp\Farm;
 
 header('Content-Type: text/plain');
 
-$text = "Set a message by adding ?message='moo' to the URL";
+$text = "https://github.com/LueYangYue/sample-php/blob/main/index.php?message=moo";
 if(isset($_GET['message']) && $_GET['message'] != '') {
 	$text = htmlspecialchars($_GET['message']);
 }
